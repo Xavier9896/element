70952bf5d1d274114fbe54d0974f7424a3ce79c7
吴老师 <1374902847@qq.com>
2022-10-27 08:39:44 +0800

    删除手动配置的虚拟列表文件

--- packages/virtual-table-column/index.js
+++
@@ -1,8 +0,0 @@
-import VirtualTableColumn from '../table/src/table-column';
-
-/* istanbul ignore next */
-VirtualTableColumn.install = function(Vue) {
-  Vue.component(VirtualTableColumn.name, VirtualTableColumn);
-};
-
-export default VirtualTableColumn;
--- src/index.js
+++ src/index.js
@@ -26,8 +26,6 @@ import Button from '../packages/button/index.js';
 import ButtonGroup from '../packages/button-group/index.js';
 import Table from '../packages/table/index.js';
 import TableColumn from '../packages/table-column/index.js';
-import VirtualTable from '../packages/virtual-table/index.js';
-import VirtualTableColumn from '../packages/virtual-table-column/index.js';
 import DatePicker from '../packages/date-picker/index.js';
 import TimeSelect from '../packages/time-select/index.js';
 import TimePicker from '../packages/time-picker/index.js';
@@ -120,8 +118,6 @@ const components = [
   ButtonGroup,
   Table,
   TableColumn,
-  VirtualTable,
-  VirtualTableColumn,
   DatePicker,
   TimeSelect,
   TimePicker,
@@ -246,8 +242,6 @@ export default {
   ButtonGroup,
   Table,
   TableColumn,
-  VirtualTable,
-  VirtualTableColumn,
   DatePicker,
   TimeSelect,
   TimePicker,
